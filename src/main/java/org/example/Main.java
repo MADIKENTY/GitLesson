0153ef9baa18eb97301f83382b0132708e55dd58
Madiyar <madiyar0026@gmail.com>
2023-05-15 00:46:00 +0600

    Created new project for able git workshop

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        mainMethodShowMessage();
+        mainMethodShowinfo();
+    }
+    private static void mainMethodShowMessage() {
+            System.out.println("Hello able QA course\n");
+        }
+     private static void mainMethodShowinfo() {
+        System.out.println("Lets learn Git");
+    }
+
+}
